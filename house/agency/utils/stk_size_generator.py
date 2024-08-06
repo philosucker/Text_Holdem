18bd6630762e7e719f50d15f73aae72d5c176bf6
ChangHo Lee <65852355+philosucker@users.noreply.github.com>
2024-08-06 18:20:42 +0900

    Delete house/agency directory

--- house/agency/utils/stk_size_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generate_random_stk_size(difficulty: str) -> int:
-    if difficulty == "easy":
-        return random.randint(100, 500)
-    elif difficulty == "hard":
-        return random.randint(5000, 10000)
-    return 0
